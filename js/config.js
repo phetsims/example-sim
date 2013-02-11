b36b5bfa5163e0ceee6e890c1f777837948c119e
Chris Malley <cmalley@pixelzoom.com>
2013-02-11 11:03:14 -0700

    sim has its own copies of contrib dependencies

--- js/config.js
+++ js/config.js
@@ -5,15 +5,15 @@ require.config( {
 
     paths: {
         // contrib
-        easel:"../../../contrib/easel-0.5.0",
-        i18n:"../../../contrib/i18n-2.0.1-phet",
-        image:"../../../contrib/image-0.2.1",
-        tpl: "../../../contrib/tpl-0.2",
+        easel:"../contrib/easel-0.5.0",
+        i18n:"../contrib/i18n-2.0.1-phet",
+        image:"../contrib/image-0.2.1",
+        tpl: "../contrib/tpl-0.2",
 
         // common
-        phetcommon:"../../../common/phetcommon/js",
-        phetcommon_html:"../../../common/phetcommon/html",
-        'easel-phet':"../../../common/easel-phet/js"
+        phetcommon:"../common/phetcommon/js",
+        phetcommon_html:"../common/phetcommon/html",
+        'easel-phet':"../common/easel-phet/js"
     },
 
     shim: {
