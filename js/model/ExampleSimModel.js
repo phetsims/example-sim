8372dc6daad264aa538c5d672a176109af05aa04
Sam Reid <reids@colorado.edu>
2013-03-21 14:42:04 -0600

    switched to commonjs imports

--- js/model/ExampleSimModel.js
+++ js/model/ExampleSimModel.js
@@ -5,30 +5,27 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
-define(
-    [
-      'easel',
-      'DOT/Dimension2',
-      'DOT/Vector2',
-      'model/BarMagnet'
-    ],
-    function( Easel, Dimension2, Vector2, BarMagnet ) {
-      "use strict";
+define( function( require ) {
+  "use strict";
+  var Easel = require( 'easel' );
+  var Dimension2 = require( 'DOT/Dimension2' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var BarMagnet = require( 'model/BarMagnet' );
 
-      function ExampleSimModel() {
-        // model elements
-        this.barMagnet = new BarMagnet( new Vector2( 0, 0 ), new Dimension2( 375, 75 ), 0 );
-      }
+  function ExampleSimModel() {
+    // model elements
+    this.barMagnet = new BarMagnet( new Vector2( 0, 0 ), new Dimension2( 375, 75 ), 0 );
+  }
 
-      // Resets all model elements
-      ExampleSimModel.prototype.reset = function() {
-        this.barMagnet.reset();
-      };
+  // Resets all model elements
+  ExampleSimModel.prototype.reset = function() {
+    this.barMagnet.reset();
+  };
 
-      // Called by the animation loop
-      ExampleSimModel.prototype.step = function() {
-        // Make model changes here.
-      };
+  // Called by the animation loop
+  ExampleSimModel.prototype.step = function() {
+    // Make model changes here.
+  };
 
-      return ExampleSimModel;
-    } );
+  return ExampleSimModel;
+} );
