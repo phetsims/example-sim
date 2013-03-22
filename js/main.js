8cda917b96b71a51540307cef7bff29087c295b8
Jonathan Olson <olsonsjc@gmail.com>
2013-03-21 22:13:07 -0700

    Using Scenery-polyfilled requestAnimationFrame

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ require(
       'fastclick',
       'PHETCOMMON/util/ImagesLoader',
       'model/ExampleSimModel',
-      'view/ExampleSimView'
+      'view/ExampleSimView',
+      'SCENERY/util/Util'
     ],
-    function( FastClick, ImagesLoader, ExampleSimModel, ExampleSimView ) {
+    function( FastClick, ImagesLoader, ExampleSimModel, ExampleSimView, Util ) {
       "use strict";
 
       //On iPad, prevent buttons from flickering 300ms after press.  See https://github.com/twitter/bootstrap/issues/3772
@@ -21,19 +22,8 @@ require(
 
       // model
       var model = new ExampleSimModel();
-
-      //Polyfill for request animation frame
-      //TODO: Factor out to common lib
-      window.requestAnimFrame = (function() {
-        return window.requestAnimationFrame ||
-               window.webkitRequestAnimationFrame ||
-               window.mozRequestAnimationFrame ||
-               window.oRequestAnimationFrame ||
-               window.msRequestAnimationFrame ||
-               function( callback ) {
-                 window.setTimeout( callback, 1000 / 60 );
-               };
-      })();
+      
+      Util.polyfillRequestAnimationFrame();
 
       // initialize the view
       function initView( imagesLoader ) {
@@ -46,7 +36,7 @@ require(
         // place the rAF *before* the render() to assure as close to
         // 60fps with the setTimeout fallback.
         (function animloop() {
-          requestAnimFrame( animloop );
+          requestAnimationFrame( animloop );
           view.performanceMonitor.begin();
           model.step();
           view.update();
