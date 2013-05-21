e5edf9aa50a00d1d2e19b70decc9fcffe5f615cb
Sam Reid <reids@colorado.edu>
2013-05-21 12:40:56 -0600

    Added version.js

---
+++ js/version.js
@@ -0,0 +1,7 @@
+// Copyright 2013, University of Colorado
+
+// Returns version identifier for the simulation.
+define( function( require ) {
+  "use strict";
+  return function() { return "0.0.0-1"; };
+} );
