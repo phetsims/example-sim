76dcc69a2907f5878f4f462e3587ca6d685335e0
Chris Malley <cmalley@pixelzoom.com>
2013-06-03 21:43:47 -0600

    increment version number

--- js/version.js
+++ js/version.js
@@ -3,5 +3,5 @@
 // Returns version identifier for the simulation.
 define( function( require ) {
   "use strict";
-  return function() { return "0.0.0-1"; };
+  return function() { return "0.0.0-2"; };
 } );
