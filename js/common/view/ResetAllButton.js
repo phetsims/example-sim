d8fcc7c928656ec44145b91c9793acd5c23652a2
Jonathan Olson <olsonsjc@gmail.com>
2013-08-08 19:51:49 -0600

    Proper circular hit area on ResetAllButton, instead of the square one.

--- js/common/view/ResetAllButton.js
+++ js/common/view/ResetAllButton.js
@@ -12,15 +12,29 @@ define( function( require ) {
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var PushButton = require( 'SUN/PushButton' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  var radius = 33; // hardcoded width/height for the circle of the reset all button
+  var radiusSquared = radius * radius;
+  var center = new Vector2( radius, radius );
 
   function ResetAllButton( callback, options ) {
     PushButton.call( this,
-      new Image( imageLoader.getImage( 'reset_button_up.png' ) ),
-      new Image( imageLoader.getImage( 'reset_button_over.png' ) ),
-      new Image( imageLoader.getImage( 'reset_button_down.png' ) ),
-      new Image( imageLoader.getImage( 'reset_button_disabled.png' ) ),
+      new ResetAllImage( imageLoader.getImage( 'reset_button_up.png' ) ),
+      new ResetAllImage( imageLoader.getImage( 'reset_button_over.png' ) ),
+      new ResetAllImage( imageLoader.getImage( 'reset_button_down.png' ) ),
+      new ResetAllImage( imageLoader.getImage( 'reset_button_disabled.png' ) ),
       callback, options );
   }
+
+  function ResetAllImage( image ) {
+    Image.call( this, image );
+  }
+  inherit( Image, ResetAllImage, {
+    containsPointSelf: function( point ) {
+      return point.distanceSquared( center ) <= radiusSquared;
+    }
+  } );
 
   return inherit( PushButton, ResetAllButton );
 } );
