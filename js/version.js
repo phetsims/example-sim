19f3eabbd52d458ce02fd67a0b7ba15c5c3a530d
Chris Malley <cmalley@pixelzoom.com>
2013-12-23 13:30:31 -0700

    remove unused vars

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 // Returns version identifier for the simulation.
-define( function( require ) {
+define( function() {
   'use strict';
   return function() { return '0.0.0-2'; };
 } );
