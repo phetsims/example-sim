604561c493b91e305219922be32417882e5b9439
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:49 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/example-sim-config.js
+++ js/example-sim-config.js
@@ -6,40 +6,40 @@
  * @author Chris Malley (PixelZoom, Inc.)
  * @author Sam Reid (PhET Interactive Simulations)
  */
-require.config(
-  {
-    // An array of dependencies to load. Useful when require is defined as a config object before require.js
-    // is loaded, and you want to specify dependencies to load as soon as require() is defined.
-    deps: [ 'example-sim-main' ],
+require.config( {
+  // An array of dependencies to load. Useful when require is defined as a config object before require.js
+  // is loaded, and you want to specify dependencies to load as soon as require() is defined.
+  deps: [ 'example-sim-main' ],
 
-    // baseUrl: don't bother trying to set it here, it is overridden by data-main in the top-level HTML file
+  // baseUrl: don't bother trying to set it here, it is overridden by data-main in the top-level HTML file
 
-    // Path mappings for module names not found directly under baseUrl. The path settings are assumed to be
-    // relative to baseUrl unless the paths setting starts with a '/' or has a URL protocol.
-    paths: {
+  // Path mappings for module names not found directly under baseUrl. The path settings are assumed to be
+  // relative to baseUrl unless the paths setting starts with a '/' or has a URL protocol.
+  paths: {
 
-      // third-party libs
-      text: '../../sherpa/text',
+    // third-party libs
+    text: '../../sherpa/text',
 
-      // PhET plugins
-      image: '../../chipper/requirejs-plugins/image',
-      string: '../../chipper/requirejs-plugins/string',
+    // PhET plugins
+    image: '../../chipper/requirejs-plugins/image',
+    string: '../../chipper/requirejs-plugins/string',
 
-      // common directories, uppercase names to identify them in require imports
-      AXON: '../../axon/js',
-      BRAND: '../../brand/js',
-      DOT: '../../dot/js',
-      JOIST: '../../joist/js',
-      KITE: '../../kite/js',
-      PHET_CORE: '../../phet-core/js',
-      PHETCOMMON: '../../phetcommon/js',
-      SCENERY: '../../scenery/js',
-      SCENERY_PHET: '../../scenery-phet/js',
-      SUN: '../../sun/js',
+    // common directories, uppercase names to identify them in require imports
+    AXON: '../../axon/js',
+    BRAND: '../../brand/js',
+    DOT: '../../dot/js',
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    PHET_CORE: '../../phet-core/js',
+    PHETCOMMON: '../../phetcommon/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SUN: '../../sun/js',
 
-      // this sim
-      EXAMPLE_SIM: '.'
-    },
+    // this sim
+    EXAMPLE_SIM: '.'
+  },
 
-    urlArgs: new Date().getTime()  // cache buster to make browser reload all included scripts
-  } );
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
+} );
