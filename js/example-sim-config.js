9efcde9c497310b1faba711b82e422f9610371c1
Chris Malley <cmalley@pixelzoom.com>
2015-05-15 16:51:10 -0600

    move all 3rd-party libs to sherpa/lib/, phetsims/sherpa#17

--- js/example-sim-config.js
+++ js/example-sim-config.js
@@ -18,7 +18,7 @@ require.config( {
   paths: {
 
     // third-party libs
-    text: '../../sherpa/text-2.0.12',
+    text: '../../sherpa/lib/text-2.0.12',
 
     // PhET plugins
     image: '../../chipper/js/requirejs-plugins/image',
