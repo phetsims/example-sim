03be68e0717f87b33026d7bc9784c7d0ae4d2022
zepumph <michael.kauzmann@colorado.edu>
2019-10-22 13:39:04 -0800

    add UTTERANCE_QUEUE to configs, move tests over to utterance-queue repo, convert usages to use the library, not the scenery-phet ones, delete those, https://github.com/phetsims/utterance-queue/issues/2

--- js/example-sim-config.js
+++ js/example-sim-config.js
@@ -40,9 +40,10 @@ require.config( {
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
     TAMBO: '../../tambo/js',
-    TANDEM: '../../tandem/js'
+    TANDEM: '../../tandem/js',
+    UTTERANCE_QUEUE: '../../utterance-queue/js'
   },
 
-  // optional cache bust to make browser refresh load all included scripts, can be enabled with ?cacheBust
+  // Cache busting is applied by default, but can be disabled via ?cacheBust=false, see initialize-globals.js
   urlArgs: phet.chipper.getCacheBustArgs()
 } );
