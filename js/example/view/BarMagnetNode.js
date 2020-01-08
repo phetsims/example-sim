7fe166d7af4dc8a467c451c600444e099ed410d6
phet-dev <phetadmin@gmail.com>
2020-01-08 03:13:12 -0700

    update copyright dates from daily grunt work

--- js/example/view/BarMagnetNode.js
+++ js/example/view/BarMagnetNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2019, University of Colorado Boulder
+// Copyright 2013-2020, University of Colorado Boulder
 
 /**
  * View for the bar magnet object, which can be dragged to translate.
