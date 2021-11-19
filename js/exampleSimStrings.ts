ff2e54c1490da66f56555712b643ee49dfd51058
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:05 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/exampleSimStrings.ts
+++ js/exampleSimStrings.ts
@@ -3,11 +3,23 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import exampleSim from './exampleSim.js';
 
-const exampleSimStrings = getStringModule( 'EXAMPLE_SIM' );
+type StringsType = {
+  'example-sim': {
+    'title': string
+  },
+  'screen': {
+    'magnets': string,
+    'particles': string
+  },
+  'magnetControls': string,
+  'flipPolarity': string
+};
+
+const exampleSimStrings = getStringModule( 'EXAMPLE_SIM' ) as StringsType;
 
 exampleSim.register( 'exampleSimStrings', exampleSimStrings );
 
