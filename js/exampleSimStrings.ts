7cac04a10d2d7f4c6c9ebfcd795f36acbf83d74c
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/exampleSimStrings.ts
+++ js/exampleSimStrings.ts
@@ -5,18 +5,24 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import exampleSim from './exampleSim.js';
 
 type StringsType = {
   'example-sim': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'magnets': string;
+    'magnetsProperty': TReadOnlyProperty<string>;
     'particles': string;
+    'particlesProperty': TReadOnlyProperty<string>;
   };
   'magnetControls': string;
+  'magnetControlsProperty': TReadOnlyProperty<string>;
   'flipPolarity': string;
+  'flipPolarityProperty': TReadOnlyProperty<string>;
 };
 
 const exampleSimStrings = getStringModule( 'EXAMPLE_SIM' ) as StringsType;
