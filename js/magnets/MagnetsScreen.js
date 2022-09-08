67d4cacf8cf7c9e88938bc7e06255550659b1ed8
phet-dev <phetadmin@gmail.com>
2022-09-08 04:24:03 -0600

    update copyright dates from daily grunt work

--- js/magnets/MagnetsScreen.js
+++ js/magnets/MagnetsScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * MagnetsScreen is the top-level component for the 'Magnets' screen.  It creates the model and view.
--- js/particles/ParticlesScreen.js
+++ js/particles/ParticlesScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * ParticlesScreen is the top-level component for the 'Particles' screen.  It creates the model and view.
