d883ee590673a19fd97f163f63f45e6f840bbd34
Franz Amador <franzamador@gmail.com>
2024-07-19 13:51:32 -0700

    Remove unused options

--- js/magnets/model/MagnetsModel.ts
+++ js/magnets/model/MagnetsModel.ts
@@ -12,27 +12,20 @@
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import exampleSim from '../../exampleSim.js';
-import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
-import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
-import TModel from '../../../../joist/js/TModel.js';
 import BarMagnet from './BarMagnet.js';
-
-type SelfOptions = EmptySelfOptions;
-type MagnetsModelOptions = SelfOptions & PickRequired<PhetioObjectOptions, 'tandem'>;
 
 export default class MagnetsModel implements TModel {
 
   // initial bar magnet model element
   barMagnet: BarMagnet;
 
-  public constructor( providedOptions: MagnetsModelOptions ) {
+  public constructor() {
     this.barMagnet = new BarMagnet( new Dimension2( 250, 50 ), new Vector2( 0, 0 ), 0 );
   }
 
   /**
    * Restores the initial state of all model elements.
    * This method is called when the simulation's "Reset All" button is pressed.
-   * @public
    */
   public reset(): void {
     this.barMagnet.reset();
