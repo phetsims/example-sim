3b5aba5145261b61788dad2c75f3b104e3bd632c
phet-dev <phetadmin@gmail.com>
2025-02-08 03:46:47 -0700

    update copyright dates from daily grunt work

--- js/particles/view/ParticlesScreenView.ts
+++ js/particles/view/ParticlesScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * ParticlesScreenView is the top-level view component for the 'Magnets' screen. All of the components that make up
