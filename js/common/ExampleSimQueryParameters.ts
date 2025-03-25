13012e11c60d7537950d87291c13610c3be36b52
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:33:14 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/ExampleSimQueryParameters.ts
+++ js/common/ExampleSimQueryParameters.ts
@@ -8,6 +8,7 @@
  */
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import exampleSim from '../exampleSim.js';
 
 const SCHEMA_MAP = {
